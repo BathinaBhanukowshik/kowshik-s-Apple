a1824c4a974d73daff9292ea0709125af837b48b
BHANUKOWSHIK BATHINA <70278761+BathinaBhanukowshik@users.noreply.github.com>
2022-05-22 12:07:55 +0530

    Add files via upload

---
+++ comment.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_from = 'kowshik@gmail.com';
+
+$email_subject = 'kowshik\'s Apple';
+
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "User Message: $message .\n";
+
+$to = 'bhanukowshik7126@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: index.html");
+?>
